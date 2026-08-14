1cdee763e58e33d92be0549106c537aab371151f
Sunmi Jung <61809936+applepple@users.noreply.github.com>
2026-08-14 16:32:27 +0000

    Fri, Aug 14, 2026, 12:32 PM -04:00

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -108,3 +108,12 @@ def get_cars(request):
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
+
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
