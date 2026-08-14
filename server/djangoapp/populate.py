1b17429178f51c73ead32324a7a741a811fb88d4
Sunmi Jung <61809936+applepple@users.noreply.github.com>
2026-08-14 14:02:47 +0000

    Fri, Aug 14, 2026, 10:02 AM -04:00

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -10,7 +10,7 @@ def initiate():
         {"name":"Kia", "description":"Great cars. Korean technology"},
         {"name":"Toyota", "description":"Great cars. Japanese technology"},
     ]
-t
+
     car_make_instances = []
     for data in car_make_data:
             car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
