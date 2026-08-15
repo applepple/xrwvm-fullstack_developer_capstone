fa77568a78c5027741649039c25315c684fe206e
Sunmi Jung <61809936+applepple@users.noreply.github.com>
2026-08-15 05:04:10 +0000

    Sat, Aug 15, 2026, 1:04 AM -04:00

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,6 @@
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
@@ -13,6 +14,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
 
     </Routes>
   );
