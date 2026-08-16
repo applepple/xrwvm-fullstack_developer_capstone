6ef673b02ce811f1a6578ee1a51abc4183926cf3
Sunmi Jung <61809936+applepple@users.noreply.github.com>
2026-08-16 23:50:27 +0000

    Sun, Aug 16, 2026, 7:50 PM -04:00

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -117,7 +117,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":endpoint})
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
