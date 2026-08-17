cbd75cdf33944836f26594391f249576c748eb6e
Sunmi Jung <61809936+applepple@users.noreply.github.com>
2026-08-17 00:01:49 +0000

    Sun, Aug 16, 2026, 8:01 PM -04:00

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -19,7 +19,7 @@ def get_request(endpoint, **kwargs):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
 
-    request_url = backend_url + endpoint + "?" + params
+    request_url = "https://sturdy-spoon-j7vv5gjpjwv256jw-3030.app.github.dev" + endpoint + "?" + params
     print("GET from {} ".format(request_url))
     print("request_url: {}".format(request_url))
     try:
